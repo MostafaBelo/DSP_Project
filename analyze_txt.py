c0443438fac590f89d526dee84f2afed5995ae42
Mostafa Elfaggal <mostafaelfaggal@gmail.com>
2024-07-19 02:26:04 +0300

    tested all possible windows

---
+++ analyze_txt.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+with open("res.txt", "r") as f:
+    data = f.read()
+
+data = [(line.split(":")[0], line.split(":")[1].split(" ->")[0], line.split(": ")[1].split(
+    " |")[0], line.split(": ")[2]) for line in data.split("\n")]
+
+data = [[int(s), int(e), int(k), float(a)] for s, e, k, a in data]
+
+data = np.array(data)
+
+i = data.argmax(axis=0)[3]
+
+print(
+    f"Start: {data[i, 0]} | End: {data[i, 1]} | K: {data[i, 2]} | Acc: {data[i, 3]}")
